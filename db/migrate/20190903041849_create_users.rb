f9cf8ea3d71130e10d6d32bfeefc3a2688b712ea
Jake Miller <jake0miller@gmail.com>
2019-09-02 22:27:11 -0600

    Create users migration, model, and registration test

---
+++ db/migrate/20190903041849_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :email, index: true
+      t.string :first_name
+      t.string :last_name
+      t.string :password_digest
+      t.integer :role, default: 0
+      t.string :registered
+
+      t.timestamps
+    end
+  end
+end
