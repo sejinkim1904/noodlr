873bc9a3d3bbdfd72c07ac699fdea67c56cd12b8
Jake Miller <jake0miller@gmail.com>
2019-09-03 16:31:45 -0600

    Write test for user activation

--- spec/features/visitor/visitor_can_register_spec.rb
+++ spec/features/visitor/visitor_can_register_spec.rb
@@ -2,14 +2,16 @@ require 'rails_helper'
 
 describe 'vister can create an account' do
   it 'visits the home page' do
+    visit '/'
+    click_on 'Sign In'
     email = 'jimbob@aol.com'
     first_name = 'Jim'
     last_name = 'Bob'
     password = 'password'
 
-    visit login_path
-    click_on 'Sign Up'
+    expect(current_path).to eq(login_path)
 
+    click_on 'Sign Up'
     fill_in 'user[email]', with: email
     fill_in 'user[first_name]', with: first_name
     fill_in 'user[last_name]', with: last_name
