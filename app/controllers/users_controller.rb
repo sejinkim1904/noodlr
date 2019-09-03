3bafb465cf510f0769317c4f42f95be4ac7f789d
Jake Miller <jake0miller@gmail.com>
2019-09-03 15:03:52 -0600

    Create registration email process

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,20 @@ class UsersController < ApplicationController
   end
 
   def create
-    redirect_to dashboard_path
+    user = User.create(user_params.merge(registered: SecureRandom.hex))
+    if user.save
+      session[:user_id] = user.id
+			flash[:notice] = "Logged in as #{user.first_name} #{user.last_name}"
+			redirect_to send_registration_path
+    else
+      flash[:error] = 'That email has already been taken.'
+      redirect_to new_user_path
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :first_name, :last_name, :password)
   end
 end
