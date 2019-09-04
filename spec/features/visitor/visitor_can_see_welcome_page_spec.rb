e864f342266074d560091e146f00f99f1ea9d185
Jake Miller <jake0miller@gmail.com>
2019-09-04 08:53:43 -0600

    Update welcome page test

--- spec/features/visitor/visitor_can_see_welcome_page_spec.rb
+++ spec/features/visitor/visitor_can_see_welcome_page_spec.rb
@@ -2,16 +2,14 @@ require 'rails_helper'
 
 describe 'Welcome Page' do
   describe 'As a visitor' do
-    describe 'When I visit "/" I see the welcome page with most popular search terms' do
-      it "I also see a 'Browse more' button" do
-        visit "/"
+    it 'I see the welcome page with popular search terms' do
+      visit root_path
 
-        expect(page).to have_content('Most Popular')
-        save_and_open_page
-        expect(page).to have_button('Login')
-        expect(page).to have_button('Search')
-        expect(page).to have_button('Browse More')
-      end
+      expect(page).to have_content('Most Popular')
+
+      expect(page).to have_button('Login')
+      expect(page).to have_button('Search')
+      expect(page).to have_button('Browse More')
     end
   end
 end
