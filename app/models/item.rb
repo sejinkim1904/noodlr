a2a40e7e9118e8f11f41438f8c711e5b18c590da
Sejin Kim <froydroyce@gmail.com>
2019-09-05 15:27:05 -0600

    Refactor validations

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
-  validates_presence_of :name
-  validates_presence_of :category
-  validates_presence_of :price
-  validates_presence_of :image
-  validates_presence_of :restaurant
+  validates_presence_of :name,
+                        :category,
+                        :price,
+                        :image,
+                        :restaurant
 end
