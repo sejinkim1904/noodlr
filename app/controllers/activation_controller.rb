054037a5434d2048adf886ed4dcff4a523a3c343
Jake Miller <jake0miller@gmail.com>
2019-09-05 15:59:04 -0600

    Add sad paths to user activation action

--- app/controllers/activation_controller.rb
+++ app/controllers/activation_controller.rb
@@ -1,13 +1,21 @@
 class ActivationController < ApplicationController
   def create
-    user = User.find_by(registered: params[:code])
+    user_data = params[:code].split('-')
+    binding.pry
+    user = User.find(user_data[0])
   	if user.nil?
   		flash[:error] = "User not found!"
   		redirect_to root_path
-  	else
+    elsif user.registered == "true"
+      flash[:notice] = "Your account is already activated!"
+      redirect_to dashobard_path
+  	elsif user.registered == user_data[1]
   		user.update_attributes(registered: "true")
       flash[:notice] = "Thank you! Your account is now activated."
       redirect_to dashboard_path
+    else
+      flash[:error] = "Invalid activation code!"
+  		redirect_to root_path
   	end
   end
 end
