a58d33431b50d542c6eabef2c3221fa7508e73d9
Sejin Kim <froydroyce@gmail.com>
2019-09-05 17:04:43 -0600

    Update validation test in item

--- spec/models/items_spec.rb
+++ spec/models/items_spec.rb
@@ -4,7 +4,7 @@ describe Item do
   describe 'validations' do
     it { should validate_presence_of :name }
     it { should validate_presence_of :price }
-    it { should validate_presence_of :dish_type }
+    it { should validate_presence_of :category }
     it { should validate_presence_of :image }
     it { should validate_presence_of :restaurant }
   end
