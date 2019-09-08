f9dbc4451e30a24b35565cab6bb846bb7910c225
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-09-08 15:01:26 -0600

    Create migration for restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.create!([
+{ "name": "Osaka", "latitude": "39.7595063","longitude": "-104.9861053"},
+{ "name": "Domo Japanese Country Food", "latitude": "39.7382478", "longitude": "-105.0055633"},
+{ "name": "Tokio", "latitude": "39.7586035", "longitude": "-104.9974398"}
+])
