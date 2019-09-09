422e6c99b55379c30d2b11e78ef6a6a36fdf8584
Jake Miller <jake0miller@gmail.com>
2019-09-09 09:53:28 -0600

    Update with master

---
+++ config/routes.rb
@@ -0,0 +1,38 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'welcome#index'
+
+  get '/register', to: 'registration#new', as: :send_registration
+  get '/dashboard', to: 'users#show', as: :dashboard
+
+  get '/signup', to: 'users#new', as: :new_user
+  get '/login', to: 'sessions#new', as: :login
+  post '/login', to: 'sessions#create', as: :logged_in
+  get '/logout', to: 'sessions#destroy', as: :logout
+  get '/activation/:code', to: 'activation#create', as: :activation
+
+  get '/facebook/login', to: 'facebook/sessions#new', as: :facebook_login
+  get '/gmail/login', to: 'gmail/sessions#new', as: :gmail_login
+  get '/instagram/login', to: 'instagram/sessions#new', as: :instagram_login
+
+  resources :users, only: [:create]
+  resources :restaurants
+
+  resources :items, only: [:show]
+
+  resources :restaurants, only: [:show]
+
+  resources :search, only: [:index]
+
+  namespace :default do
+    resources :items do
+      resources :reviews, only: [:new, :create]
+    end
+
+    resources :restaurants do
+      resources :items, only: [:new, :create]
+    end
+  end
+
+  match '*path', to: 'welcome#index', via: :all
+end
