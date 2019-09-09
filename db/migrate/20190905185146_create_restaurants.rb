a0eb993fb3785f9e2b6977813a8f84f8af119a08
Sejin Kim <froydroyce@gmail.com>
2019-09-09 14:27:53 -0600

    Update restaurant table with attributes from yelp API

--- db/migrate/20190905185146_create_restaurants.rb
+++ db/migrate/20190905185146_create_restaurants.rb
@@ -1,9 +1,17 @@
 class CreateRestaurants < ActiveRecord::Migration[5.2]
   def change
     create_table :restaurants do |t|
+      t.string :yelp_id
+      t.decimal :rating
       t.string :name
       t.decimal :latitude
       t.decimal :longitude
+      t.string :address_1
+      t.string :address_2
+      t.string :city
+      t.string :state
+      t.string :zip_code
+      t.string :phone
 
       t.timestamps
     end
