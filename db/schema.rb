2a1b303714f17a1a04cf48d134525f71d62b9f9e
Jake Miller <jake0miller@gmail.com>
2019-09-09 09:55:42 -0600

    Migrate with new schema

--- db/schema.rb
+++ db/schema.rb
@@ -29,10 +29,8 @@ ActiveRecord::Schema.define(version: 2019_09_07_225259) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
-    t.string "description"
-    t.string "latitude"
-    t.string "longitude"
-    t.integer "price"
+    t.decimal "latitude"
+    t.decimal "longitude"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
