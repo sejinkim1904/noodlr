d75a2bf6eeb13236dccace8e38dc6098271e460c
Sejin Kim <froydroyce@gmail.com>
2019-09-09 15:51:46 -0600

    Update restaurant spec to validate new attributes

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,15 @@
 class Restaurant < ApplicationRecord
-  validates_presence_of :name, :latitude, :longitude
+  validates_presence_of :name,
+                        :rating,
+                        :latitude,
+                        :longitude,
+                        :address_1,
+                        :address_2,
+                        :city,
+                        :state,
+                        :zip_code,
+                        :phone
+                        
+  validates :yelp_id, presence: true, uniqueness: true
   has_many :items
 end
