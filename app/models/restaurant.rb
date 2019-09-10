6ed4a998ef7755c948c5531983824ff23cf945dd
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-09-10 15:48:11 -0600

    Add required arguments for reverse geocoding a restaurant with geocoder gem

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,7 @@
 class Restaurant < ApplicationRecord
+  reverse_geocoded_by :latitude, :longitude
+  after_validation :reverse_geocode
+  
   validates_presence_of :name, :latitude, :longitude
   has_many :items
 end
