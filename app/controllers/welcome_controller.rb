cd7b54aa27f203529835ec06ca5b725634b6044b
Sejin Kim <froydroyce@gmail.com>
2019-09-11 16:27:36 -0600

    Add instance variable for items by average rating

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,6 @@
 class WelcomeController < ApplicationController
   def index
+    @items_by_rating = Item.by_avg_rating
     coordinates = location.coordinates
     @restaurants = Restaurant.near(coordinates, 5)
   end
