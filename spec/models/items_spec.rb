fa69f2c837a4502cd3a6b46c0f4cd106e2b9a2a7
Sejin Kim <froydroyce@gmail.com>
2019-09-11 13:44:57 -0600

    Create by_avg_rating test

--- spec/models/items_spec.rb
+++ spec/models/items_spec.rb
@@ -18,13 +18,25 @@ describe Item, search: true do
     before :each do
       @user = User.create!(registered: 'true')
       @restaurant = create(:restaurant)
-      @ramen = @restaurant.items.create!(name: "Tonkotsu", price: 14, category: "ramen", image: "https://s3-media2.fl.yelpcdn.com/bphoto/eMmvwCMQtOUs7GxccJu4zA/o.jpg", status: 'verified')
-      @ramen.reviews.create!(user: @user, title: 'So tasteful!', body: 'This is the tastiest Tonkotsu in town', rating: 5)
-      @ramen.reviews.create!(user: @user, title: 'So tasteful!', body: 'This is the tastiest Tonkotsu in town', rating: 3)
+      @ramen_1 = @restaurant.items.create!(name: "Tonkotsu", price: 14, category: "ramen", image: "https://s3-media2.fl.yelpcdn.com/bphoto/eMmvwCMQtOUs7GxccJu4zA/o.jpg", status: 'verified')
+      @ramen_2 = @restaurant.items.create!(name: "Shoyu", price: 14, category: "ramen", image: "https://s3-media2.fl.yelpcdn.com/bphoto/eMmvwCMQtOUs7GxccJu4zA/o.jpg", status: 'verified')
+      @ramen_3 = @restaurant.items.create!(name: "Miso", price: 14, category: "ramen", image: "https://s3-media2.fl.yelpcdn.com/bphoto/eMmvwCMQtOUs7GxccJu4zA/o.jpg", status: 'verified')
+      @ramen_4 = @restaurant.items.create!(name: "Tsukemen", price: 14, category: "ramen", image: "https://s3-media2.fl.yelpcdn.com/bphoto/eMmvwCMQtOUs7GxccJu4zA/o.jpg", status: 'verified')
+      @ramen_5 = @restaurant.items.create!(name: "Shio", price: 14, category: "ramen", image: "https://s3-media2.fl.yelpcdn.com/bphoto/eMmvwCMQtOUs7GxccJu4zA/o.jpg", status: 'verified')
+      @ramen_1.reviews.create!(user: @user, title: 'So tasteful!', body: 'This is the tastiest Tonkotsu in town', rating: 5)
+      @ramen_1.reviews.create!(user: @user, title: 'So tasteful!', body: 'This is the tastiest Tonkotsu in town', rating: 3)
+      @ramen_2.reviews.create!(user: @user, title: 'So tasteful!', body: 'This is the tastiest Tonkotsu in town', rating: 2)
+      @ramen_3.reviews.create!(user: @user, title: 'So tasteful!', body: 'This is the tastiest Tonkotsu in town', rating: 3)
+      @ramen_4.reviews.create!(user: @user, title: 'So tasteful!', body: 'This is the tastiest Tonkotsu in town', rating: 1)
+      @ramen_5.reviews.create!(user: @user, title: 'So tasteful!', body: 'This is the tastiest Tonkotsu in town', rating: 5)
     end
 
     it 'average rating' do
-      expect(@ramen.average_rating).to eq(4)
+      expect(@ramen_1.average_rating).to eq(4)
+    end
+
+    it "by_avg_rating" do
+      expect(Item.by_avg_rating).to eq([@ramen_5, @ramen_1, @ramen_3, @ramen_2, @ramen_4])
     end
 
     it "search" do
