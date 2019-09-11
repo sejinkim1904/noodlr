14febb9f52617517ed10f4a2e9997cd24ba0051c
Jake Miller <jake0miller@gmail.com>
2019-09-11 16:34:49 -0600

    Update welcome_controller.rb

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
   def index
     @items_by_rating = Item.by_avg_rating
-    coordinates = location.coordinates
-    @restaurants = Restaurant.near(coordinates, 5)
+    @coordinates = location.coordinates
+    @restaurants = Restaurant.near(@coordinates, 5)
   end
 end
