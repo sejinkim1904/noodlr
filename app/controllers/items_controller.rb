41580180a4f0536c83501edea6d207a99f78fa0a
Jake Miller <jake0miller@gmail.com>
2019-09-12 20:05:52 -0600

    Add reviews to item show page

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,15 +1,5 @@
 class ItemsController < ApplicationController
   def show
-    @item = Item.includes(:restaurant).find(params[:id])
+    @item = Item.includes(:restaurant, :reviews).find(params[:id])
   end
-
-  # def autocomplete
-  #   render json: Item.search(params[:query], {
-  #     fields: ["name^5"],
-  #     match: :word_start,
-  #     limit: 10,
-  #     load: false,
-  #     misspellings: {below: 5}
-  #   }).map(&:name)
-  # end
 end
